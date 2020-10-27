0176ee84f493055dbbf184f62becc93df62127f5
noaholivier <24nolivier@athenian.org>
2020-10-27 09:25:45 -0700

    Day 1 notes

--- my_code.py
+++ my_code.py
@@ -1,3 +1,10 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
+name = 'cold'
+name2 = 'tired'
+print(f'I am {name} and {name2}')
+
+
+
+
 
